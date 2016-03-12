032ee42b3458510c036e1d0500a1f83af5503dc6
yhzzr <Heng@Hengs-iMac.local>
2016-03-11 19:47:25 -0800

    Flavors add

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidTest.java
@@ -4,12 +4,20 @@ import android.content.Context;
 import android.test.AndroidTestCase;
 import android.util.Pair;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Heng on 3/11/16.
  */
 public class AndroidTest extends AndroidTestCase {
 
     public void testFetchTask(){
-        new FetchJokeTask().execute(new Pair<Context, String>(getContext(), "Joke"));
+        try {
+            FetchJokeTask jokeTask = new FetchJokeTask();
+            jokeTask.execute(new Pair<Context, String>(getContext(), "Joke"));
+            String joke = jokeTask.get(30, TimeUnit.SECONDS);
+        } catch (Exception e){
+            fail("Timed out");
+        }
     }
 }
